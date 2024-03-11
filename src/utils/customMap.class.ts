46d163dded4252449c9e2fa5c18289e2b3ddc56d
bartick <bartickmaiti1998@gmail.com>
2024-03-11 18:09:58 +0530

    fix: bug

--- src/utils/customMap.class.ts
+++ src/utils/customMap.class.ts
@@ -23,17 +23,21 @@ export class Collection<K, V> extends Map<K, V> {
         const regex = /^\d+:/;
 
         for(let i=0; i<messageArray.length; i++) {
-            if (regex.test(messageArray[i])) {
-                if (value !== '') {
+            if(messageArray[i] == '') {
+                continue;
+            }
+
+            if(regex.test(messageArray[i])) {
+                if(value !== '') {
                     this.set(key, value);
                     value = '' as unknown as V;
                 }
                 key = messageArray[i].split(':')[0] as unknown as K;
+                value = messageArray[i].split(':')[1] as unknown as V;
             } else {
                 value = value + '\n' + messageArray[i] as unknown as V;
             }
         }
-
 
         if (value !== '') {
             this.set(key as K, value as V);
@@ -46,3 +50,13 @@ export class Collection<K, V> extends Map<K, V> {
 
     parseCopy(message: string) {}
 }
+
+const collection = new Collection<string, string>();
+
+const data = `
+9: Hello there
+This is the followup line
+8: How are you?
+7: I'm fine, thanks`
+
+console.log(collection.parseMessage(data));
